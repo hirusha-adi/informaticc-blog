8f0e21159d7785b53b9b0bdf019209b81f582395
hirusha-adi <hirushaadi@gmail.com>
2025-03-13 12:12:15 +0530

    add local search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 // See: https://docusaurus.io/docs/api/docusaurus-config
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from 'prism-react-renderer';
 
 
 /** @type {import('@docusaurus/types').Config} */
@@ -30,44 +30,6 @@ const config = {
     locales: ['en'],
   },
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        // RE_ENABLE_DOCS:
-        //    to enable docs, uncomment this section
-        // ---------------------------------------
-        // docs: {
-        //   sidebarPath: './sidebars.js',
-        //   // Please change this to your repo.
-        //   // Remove this to remove the "edit this page" links.
-        //   editUrl:
-        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        // },
-        // ---------------------------------------
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      }),
-    ],
-  ],
-
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
@@ -90,7 +52,7 @@ const config = {
           //   label: 'Tutorial',
           // },
           // ---------------------------------------
-          {to: '/blog', label: 'Blog', position: 'left'},
+          { to: '/blog', label: 'Blog', position: 'left' },
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
@@ -163,6 +125,63 @@ const config = {
         defaultLanguage: 'python',
       },
     }),
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        // RE_ENABLE_DOCS:
+        //    to enable docs, uncomment this section
+        // ---------------------------------------
+        // docs: {
+        //   sidebarPath: './sidebars.js',
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        // },
+        // ---------------------------------------
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  plugins: [
+    // ... Your other plugins.
+    [
+      require.resolve('docusaurus-plugin-search-local'),
+      {
+        // Options: https://github.com/gabrielcsapo/docusaurus-plugin-search-local?tab=readme-ov-file#plugin-options
+        hashed: true,
+
+        indexBlog: true,
+        blogRouteBasePath: "/blog",
+        blogDir: "blog",
+
+        indexDocs: true,
+        docsRouteBasePath: "/docs",
+        docsDir: "docs",
+      },
+    ],
+  ],
 };
 
 export default config;
